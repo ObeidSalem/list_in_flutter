24826a34d4db5fd472f2271c74f254277ce03774
ObeidSalem <o.salem322@gmail.com>
2021-11-17 12:36:26 +0530

    styles has been enhanced

--- lib/main.dart
+++ lib/main.dart
@@ -30,20 +30,17 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var fruitIndex = 0;
-  String dropDownValue = "Choose Fruit";
+  String dropDownValue = "Select Fruit";
 
   @override
-  var fruitList = [" ", "Apple", "Orange", "Rambutan"];
+  var fruitList = ["No Fruit", "Apple", "Orange", "Rambutan"];
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEFEFEF),
         primarySwatch: Colors.purple,
         // fontFamily: 'Georgia',
         textTheme: const TextTheme(
-          headline1: TextStyle(
-              fontSize: 72.0,
-              fontWeight: FontWeight.bold
-          ),
           headline6: TextStyle(
               fontSize: 26.0,
               fontStyle: FontStyle.italic
@@ -53,82 +50,150 @@ class MyAppState extends State<MyApp> {
               fontFamily: 'Hind'
           ),
         ),
-        // elevatedButtonTheme: ElevatedButtonThemeData(
-        //   style: ElevatedButton.styleFrom(
-        //     textStyle: const TextStyle(
-        //       fontSize: 30,
-        //     ),
-        //   ),
-        // ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: const TextStyle(
+              fontSize: 24,
+            ),
+          ),
+        ),
       ),
+
+
+
       home: Scaffold(
         appBar: AppBar(
           title: Text("Fruit List App"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('${fruitList[fruitIndex]}\n\n'),
-              DropdownButton<String>(
-                value: dropDownValue,
-                icon: const Icon(Icons.arrow_downward),
-                iconSize: 34,
-                elevation: 16,
-                style: const TextStyle(color: Colors.purple, fontSize: 25),
-                underline: Container(
-                  height: 2,
-                  color: Colors.purple,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.white10,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),                padding: EdgeInsets.all(40),
+                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 00),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('${fruitList[fruitIndex]}',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                items: <String>['Choose Fruit', "Apple", "Orange", "Rambutan"].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropDownValue = newValue!;
-                    if (newValue == 'Choose Fruit')
-                      fruitIndex = 0;
-                    else if (newValue==fruitList[1])
-                      fruitIndex = 1;
-                    else if (newValue==fruitList[2])
-                      fruitIndex = 2;
-                    else if (newValue==fruitList[3])
-                      fruitIndex = 3;
-                    else
-                      fruitIndex = 0;
-                  });
-                },              )
-              // ElevatedButton(child: Text('   apple    '), onPressed: () => {
-              //   setState(() {
-              //     fruitIndex = 1;
-              //   }),
-              //   print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
-              // }),
-              // ElevatedButton(child: Text('  orange   '),onPressed: () => {
-              //   setState(() {
-              //     fruitIndex = 2;
-              //   }),
-              //   print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
-              // }),
-              // ElevatedButton(child: Text('rambutan'),onPressed: () => {
-              //   setState(() {
-              //     fruitIndex = 3;
-              //   }),
-              //   print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
-              // }),
-              // ElevatedButton(child: Text('    Clear    '),onPressed: () => {
-              //   setState(() {
-              //     fruitIndex = 0;
-              //   }),
-              //   print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
-              // }),
-            ],
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 150, 20, 140),
+                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 00),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.white10,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      DropdownButton<String>(
+                        value: dropDownValue,
+                        icon: const Icon(Icons.arrow_downward),
+                        iconSize: 34,
+                        elevation: 16,
+                        style: const TextStyle(color: Colors.purple, fontSize: 25),
+                        underline: Container(
+                          height: 2,
+                          color: Colors.purple,
+                        ),
+                        items: <String>['Select Fruit', "Apple", "Orange", "Rambutan"].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropDownValue = newValue!;
+                            if (newValue==fruitList[1])
+                              fruitIndex = 1;
+                            else if (newValue==fruitList[2])
+                              fruitIndex = 2;
+                            else if (newValue==fruitList[3])
+                              fruitIndex = 3;
+                            else
+                              fruitIndex = 0;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 00),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.white10,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(child: Text('   apple    '), onPressed: () => {
+                            setState(() {
+                              fruitIndex = 1;
+                            }),
+                            print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
+                          }),
+
+                          ElevatedButton(child: Text('  orange   '),onPressed: () => {
+                            setState(() {
+                              fruitIndex = 2;
+                            }),
+                            print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
+                          }),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(child: Text('rambutan'),onPressed: () => {
+                            setState(() {
+                              fruitIndex = 3;
+                            }),
+                            print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
+                          }),
+
+                          ElevatedButton(child: Text('    Clear    '),onPressed: () => {
+                            setState(() {
+                              fruitIndex = 0;
+                            }),
+                            print('Button Pressed, fruitIndex value is ${fruitIndex} now'),
+                          }),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
